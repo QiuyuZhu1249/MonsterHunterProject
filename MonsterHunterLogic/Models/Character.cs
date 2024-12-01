ff47efa26c9e15e54da58235ffe92a341b264c31
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 02:51:12 -0500

    Define common properties and methods for roles.

--- MonsterHunterLogic/Models/Character.cs
+++ MonsterHunterLogic/Models/Character.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Models
 {
-    internal class Character
+    public abstract class Character
     {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int MaxHP { get; private set; }
+        public int CurrentHP { get; private set; }
+        public int Strength { get; private set; }
+        public int Armor { get; private set; }
+        public int FreezeTime { get; protected set; }
+
+        protected Character(int x, int y, int maxHP, int strength, int armor)
+        {
+            X = x;
+            Y = y;
+            MaxHP = maxHP;
+            CurrentHP = maxHP;
+            Strength = strength;
+            Armor = armor;
+        }
+
+        public abstract bool Move(int newX, int newY); 
+
+        public bool IsDead() => CurrentHP <= 0;
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHP = Math.Max(0, CurrentHP - damage);
+        }
     }
 }
