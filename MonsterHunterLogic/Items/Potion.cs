3f42f86848dd8e5af0a9ff896d50a8da8b38b3d3
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 14:26:44 -0500

    Add the system to confirm the type of the potions

--- MonsterHunterLogic/Items/Potion.cs
+++ MonsterHunterLogic/Items/Potion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonsterHunterLogic.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Items
 {
-    internal class Potion
+    public class Potion
     {
+        public PotionType Type { get; private set; }
+
+        public Potion()
+        {
+            //get random numbers to confirm the type of potions
+            int roll = RandomNumber.Next(1, 7);
+            switch (roll)
+            {
+                case 1:
+                    Type = PotionType.Poisoned;
+                    break;
+                case 2:
+                case 3:
+                    Type = PotionType.Speed;
+                    break;
+                case 4:
+                case 5:
+                    Type = PotionType.Invisibility;
+                    break;
+                case 6:
+                    Type = PotionType.Strength;
+                    break;
+                default:
+                    Type = PotionType.Strength;
+                    break;
+            }
+        }
     }
 }
