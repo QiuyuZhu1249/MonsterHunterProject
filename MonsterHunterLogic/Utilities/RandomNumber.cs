a0edf37ccd60139bc543705342e21b90acff8d7c
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 02:29:54 -0500

    Finish the coding of getting random number

--- MonsterHunterLogic/Utilities/RandomNumber.cs
+++ MonsterHunterLogic/Utilities/RandomNumber.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Utilities
 {
-    internal class RandomNumber
+    public class RandomNumber
     {
+        private static readonly Random random = new Random();
+
+        private RandomNumber() { }
+
+        public static int Next(int min, int max)
+        {
+            return random.Next(min, max);
+        }
     }
 }
