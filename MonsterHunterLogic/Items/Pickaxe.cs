66c227994e3e560970c3f146d206ba44c284476f
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 14:08:21 -0500

    Add the using system of the pickaxe

--- MonsterHunterLogic/Items/Pickaxe.cs
+++ MonsterHunterLogic/Items/Pickaxe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonsterHunterLogic.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Items
 {
-    internal class Pickaxe
+    public class Pickaxe
     {
+        public Pickaxe() { }
+
+        public bool Use()
+        {
+            //get random percentage break the pickaxe
+            return RandomNumber.Next(1, 4) != 1;
+        }
     }
 }
