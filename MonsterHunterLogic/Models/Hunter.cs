ab479fcf018779219fe398f160dbb281aefa8e9b
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 03:34:36 -0500

    Complete Hunter.cs code but get two errors, try to fix

--- MonsterHunterLogic/Models/Hunter.cs
+++ MonsterHunterLogic/Models/Hunter.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Models
 {
-    internal class Hunter
+    public class Hunter : Character
     {
+        public string Name { get; set; }
+        public int Score { get; private set; }
+
+        // Write setting to the hunter HP, ATK, DEF
+        public Hunter(int x, int y, string name)
+            : base(x, y, 30, 7, 4) 
+        {
+            Name = name.Length <= 20 ? name : name.Substring(0, 20);
+            FreezeTime = 1000; 
+            Score = 0;
+        }
+
+        public void AddScore(int points) => Score += points;
+
+        public override bool Move(int newX, int newY)
+        {
+            // Movement
+            X = newX;
+            Y = newY;
+            return true;
+        }
     }
 }
