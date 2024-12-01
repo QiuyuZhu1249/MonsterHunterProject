5693855738ff2850902faade90ec745f98786669
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 13:56:56 -0500

    Add the using system of the sword

--- MonsterHunterLogic/Items/Sword.cs
+++ MonsterHunterLogic/Items/Sword.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonsterHunterLogic.Utilities;
 
 namespace MonsterHunterLogic.Items
 {
-    internal class Sword
+    public class Sword
     {
+        public int Strength { get; private set; }
+
+        public Sword()
+        {
+            // get random ATK 4-9
+            Strength = RandomNumber.Next(4, 10); 
+        }
+
+        public bool Use()
+        {
+            //get random percentage break the sword
+            return RandomNumber.Next(1, 6) != 1;
+        }
     }
 }
