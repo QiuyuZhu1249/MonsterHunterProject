6c51d5b9b4e4e22d9908868d0b541ccb72919fc5
qzhu1249@gmail.com <qzhu1249@gmail.com>
2024-12-01 23:12:23 -0500

    Created the system that can load the map

--- MonsterHunterConsole/Controller/GameController.cs
+++ MonsterHunterConsole/Controller/GameController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonsterHunterLogic.Utilities;
 
 namespace MonsterHunterConsole.Controller
 {
     public class GameController
     {
         private bool isRunning;
-        private string[] maps = { "Castle", "Marsh", "Hell" };
+        private string[] maps = { "Castle.map", "Marsh.map", "Hell.map" };
 
         public void Start()
         {
@@ -55,12 +56,33 @@ namespace MonsterHunterConsole.Controller
                 else if (input == ConsoleKey.D1 || input == ConsoleKey.D2 || input == ConsoleKey.D3)
                 {
                     int mapIndex = input - ConsoleKey.D1;
-                    Console.Clear();
-                    Console.WriteLine($"You choose:{maps[mapIndex]}");
-                    Console.WriteLine("Press any button to Quit...");
-                    Console.ReadKey();
+                    string selectedMap = maps[mapIndex];
+
+                    LoadAndDisplayMap(selectedMap);
+
                     ShowMapSelectionScreen();
                 }
+            }
+        }
+
+        private void LoadAndDisplayMap(string mapFile)
+        {
+            Map map = new Map();
+
+            try
+            {
+                map.LoadMap(mapFile); // 加载地图文件
+                Console.Clear();
+                Console.WriteLine($"Loading the map：{mapFile}");
+                map.DisplayMap(); // 显示地图内容
+                Console.WriteLine("\nPress any button to go back...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed loading the map：{ex.Message}");
+                Console.WriteLine("\nPress any button to go back...");
+                Console.ReadKey();
             }
         }
 
--- MonsterHunterLogic/Utilities/Map.cs
+++ MonsterHunterLogic/Utilities/Map.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonsterHunterLogic.Utilities
 {
-    internal class Map
+    public class Map
     {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public char[,] MapData { get; private set; }
+
+        public void LoadMap(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"地图文件未找到：{filePath}");
+
+            string[] lines = File.ReadAllLines(filePath);
+            Height = lines.Length;
+            Width = lines[0].Length;
+
+            MapData = new char[Height, Width];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    MapData[i, j] = lines[i][j];
+                }
+            }
+        }
+
+        public void DisplayMap()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write(MapData[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
